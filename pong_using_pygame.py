89c291a81338412f9558f1de5bbe7d4481460b65
Shivam Garg <gargshivam34has@gmail.com>
2023-03-18 17:59:42 +0530

    Scoreboard functional in Game, added timer

--- pong_using_pygame.py
+++ pong_using_pygame.py
@@ -14,36 +14,67 @@ PADDLE_LENGTH = 140
 PADDLE_WIDTH = 10
 OFFSET = 20
 OFFSET_OPPONENT = 10
-BALL_SPEED_X = 5
-BALL_SPEED_Y = 5
+INIT_BALL_SPD_X = 5
+INIT_BALL_SPD_Y = 5
+BALL_SPEED_X = INIT_BALL_SPD_X
+BALL_SPEED_Y = INIT_BALL_SPD_Y
 PADDLE_SPEED = 0
 PADDLE_SPEED_STEP = 8
-OPPONENT_SPEED = 20 ### opponent ai paddle speed
+OPPONENT_SPEED = 8 ### opponent ai paddle speed
 PLAYER_SCORE=0
 OPPONENT_SCORE=0
-SCOREBOARD_FONT=pygame.font.SysFont("Arial",30)
+SCOREBOARD_FONT=pygame.font.SysFont("Consolas",30)
+
+SCORE_TIME = None
 
 BALL_SPEED_X *= random.choice((1,-1))
 BALL_SPEED_Y *= random.choice((1,-1))
 
 def ball_motion(BALL_SPEED_X, BALL_SPEED_Y):
+    global PLAYER_SCORE, OPPONENT_SCORE, SCORE_TIME
     # moving the ball
     ball.x += BALL_SPEED_X
     ball.y += BALL_SPEED_Y
     # collisions
     if ball.top <= 0 or ball.bottom >= SCREEN_HEIGHT:  # vertical boundary
         BALL_SPEED_Y *= -1
-    if ball.left <= 0 or ball.right >= SCREEN_WIDTH:  # horizontal boundary
+    if ball.left <= 0:  # horizontal boundary
         #BALL_SPEED_X *= -1 # uncomment for normal boring gameplay...
-        BALL_SPEED_X,BALL_SPEED_Y = ball_reset(BALL_SPEED_X,BALL_SPEED_Y)
+        PLAYER_SCORE+=1
+        SCORE_TIME = pygame.time.get_ticks()
+
+    if ball.right >= SCREEN_WIDTH:  # horizontal boundary
+        #BALL_SPEED_X *= -1 # uncomment for normal boring gameplay...
+        OPPONENT_SCORE += 1
+        SCORE_TIME = pygame.time.get_ticks()
+
     if ball.colliderect(player) or ball.colliderect(opponent):
         BALL_SPEED_X *= -1
     return BALL_SPEED_X, BALL_SPEED_Y
 
 def ball_reset(BALL_SPEED_X,BALL_SPEED_Y):
+    global SCORE_TIME
     ball.center = (SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
-    BALL_SPEED_X *= random.choice((1, -1))
-    BALL_SPEED_Y *= random.choice((1, -1))
+
+    CURRENT_TIME=pygame.time.get_ticks() # current_instantaneous time
+
+    time_offset_ms=700
+    init_time=0
+    counter_steps=3
+
+    while counter_steps != 0:
+        if init_time < (CURRENT_TIME - SCORE_TIME) < (init_time + time_offset_ms):
+            no_3 = SCOREBOARD_FONT.render(f"{counter_steps}", True, light_grey)
+            screen.blit(no_3, (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2))
+        init_time += time_offset_ms
+        counter_steps -= 1
+
+    if CURRENT_TIME - SCORE_TIME < 2100:
+        BALL_SPEED_X, BALL_SPEED_Y = 0, 0
+    else:
+        BALL_SPEED_X = INIT_BALL_SPD_X * random.choice((1, -1))
+        BALL_SPEED_Y = INIT_BALL_SPD_Y * random.choice((1, -1))
+        SCORE_TIME = None
     return BALL_SPEED_X,BALL_SPEED_Y
 
 
@@ -56,9 +87,11 @@ def player_movement():
 
 
 def opponent_movement_ai():
-    if opponent.top < ball.y:
+    #dif_fact=random.choice((OPPONENT_SPEED,0))
+    dif_fact=0
+    if opponent.top + dif_fact < ball.y:
         opponent.top += OPPONENT_SPEED
-    if opponent.bottom > ball.y:
+    if opponent.bottom - dif_fact > ball.y:
         opponent.bottom -= OPPONENT_SPEED
     if opponent.top <= 0:
         opponent.top = 0
@@ -96,6 +129,8 @@ while True:
             if event.key == pygame.K_UP:
                 PADDLE_SPEED += PADDLE_SPEED_STEP
 
+
+
     #logic for game
     BALL_SPEED_X, BALL_SPEED_Y = ball_motion(BALL_SPEED_X, BALL_SPEED_Y)  # function call for ball movement
     player_movement()  # function call for player paddle movement
@@ -108,7 +143,10 @@ while True:
     pygame.draw.ellipse(screen, light_grey, ball)
     pygame.draw.aaline(screen, light_grey, (SCREEN_WIDTH / 2, 0), (SCREEN_WIDTH / 2, SCREEN_HEIGHT))
 
-    #rendering the scores (use another surface for the same then render it on the screen.)
+    if SCORE_TIME:
+        BALL_SPEED_X, BALL_SPEED_Y = ball_reset(BALL_SPEED_X, BALL_SPEED_Y)
+
+    # rendering the scores (use another surface for the same then render it on the screen.)
     player_text = SCOREBOARD_FONT.render(f"{PLAYER_SCORE}", True, light_grey)
     opponent_text = SCOREBOARD_FONT.render(f"{OPPONENT_SCORE}", True, light_grey)
     screen.blit(player_text, (700, 0))
